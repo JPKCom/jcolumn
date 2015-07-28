85b1e53106a081cc968a714f28595e1931fe205c
oliver-j <jessnerol@stud.sbg.ac.at>
2015-07-28 18:04:51 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # jcolumn
 A jQuery plugin to make selected html elements the same height.
+
+###Without jcolumn
+
+![without jcolumn screenshot](http://oliverj.net/img/jcolumn/without-jcolumn.png)
+
+###With jcolum
+![with jcolumn screenshot](http://oliverj.net/img/jcolumn/with-jcolumn.png)
+
+##Install
+-----
+
+###Bower
+```html
+bower install jcolumn
+```
+
+###The oldschool way
+Reference the JavaScript file manually directly after [jQuery](http://jquery.com):
+
+```html
+<script src="jcolumn.min.js" type="text/javascript" charset="utf-8"></script>
+```
+
+##Usage
+-----
+
+
+Just invoke jcolumn on a class of elements.
+
+```javascript
+$('.col').jColumn();
+```
+
+##Options
+-----
+
+option   | default | Description
+-------- | ------- | -----------
+delay    | 500     | The delayed time after the resize happens
+maxWidth | 767     | Every document width smaller than maxWidth will not use jColumn
+
+```javascript
+$('.col').jcolumn({
+    delay: 500,
+    maxWidth: 767
+});
+```
+
+##Authors 
+-------
+
+Oliver Jessner [@oliverj_net](https://twitter.com/oliverj_net), [Website](http://oliverj.net) 
+
+Copyright © 2015
---
+++ jcolumn.js
@@ -0,0 +1,48 @@
+// version 0.0.1
+;(function ($, win, doc) { 'use strict';
+    $.fn.jcolumn = function (options) { 
+                
+    var elements = this,
+    
+    defaults = {
+        delay: 500,
+        maxWidth: 767
+    }, 
+    
+    settings = $.extend( {}, defaults, options),
+    
+    // otherwise the event would trigger the function to fast
+    delay = (function () {
+		var timer = 0;
+	  	return function(callback, ms){
+	    	clearTimeout(timer);
+	    	timer = setTimeout(callback, ms);
+	  	};
+	})(), 
+
+	resizeColumn = function (rows) { 
+		var elemH = 0;
+            
+		if ($(doc).width() >= settings.maxWidth) {
+            rows.each(function () {
+			    var $this = $(this);
+			    $this.height('auto');
+                
+			    if ($this.height() > elemH) {
+				    elemH = $this.height();
+			    }
+		    });
+            
+			rows.each(function () {
+				$(this).height(elemH);
+			});
+		}
+	};
+    
+	win.addEventListener('resize', function() {
+		delay(resizeColumn(elements), settings.delay);
+	});
+    
+    resizeColumn(elements); // init
+    return this;
+};}(jQuery, window, document));
